c86ad21348a6cc4b6c4099abdcdd9b92ac5c897e
ximesosa24 <ximenasosa999@gmail.com>
2024-07-14 19:44:22 -0300

    changes in html, css and add functions in javascript

--- main.js
+++ main.js
@@ -6,15 +6,14 @@ const url = document.getElementById('url-link'); //Input de la URL
 const brightness = document.getElementById('brightness'); //Filter brightness
 const opacity = document.getElementById('opacity'); //Filter opacity
 const contrast = document.getElementById('contrast'); //Filter constrast
-const blur = document.getElementById('blur'); //Filter blur
+const blurFilter = document.getElementById('blur'); //Filter blur
 const grayscale = document.getElementById('grayscale'); //Filter grayscale
 const sepia = document.getElementById('sepia'); //Filter sepia
 const hue = document.getElementById('hue'); //Filter hue
 const saturated = document.getElementById('saturation'); //Filter saturated
-const Hue = document.getElementById('HUE'); //Filter hue
 const negative = document.getElementById('negative') //Filter negative
 const resetFilters = document.getElementById('reset-filters'); //Btn reset filters
-
+const imgMeme = document.getElementById('img-meme'); 
 
 //Event modo oscuro//
 btn.addEventListener('click', function(){
@@ -42,43 +41,60 @@ document.querySelector('.btn-close').addEventListener('click', function() {
 //Function de URL
 url.addEventListener('keyup', () => {
   const valueUrl = url.value;
-  canvaMeme.style.backgroundImage = `url('${valueUrl}')`;
-  canvaMeme.style.backgroundPosition = 'center';
-  canvaMeme.style.backgroundRepeat = 'no-repeat';
-  canvaMeme.style.backgroundSize = 'contain';
+  imgMeme.style.backgroundImage = `url('${valueUrl}')`;
+  imgMeme.style.backgroundPosition = 'center';
+  imgMeme.style.backgroundRepeat = 'no-repeat';
+  imgMeme.style.backgroundSize = 'contain';
 });
 
-//Apply filters
-// function applyFilters() {
-//   const filters = `
-//     brightness(${brightness.value}%)
-//     opacity(${opacity.value}%)
-//     contrast(${contrast.value}%)
-//     blur(${blurFilter.value}px)
-//     grayscale(${grayscale.value}%)
-//     sepia(${sepia.value}%)
-//     hue(${hue.value}deg)
-//     saturate(${saturated.value}%)
-//     hue(${hue.value}%)
-//     brightness(${100 - negative.value}%)
-//   `;
-//   canvaMeme.style.filter = filters;
-// }
+//Function añadir filtros
+function addFilters() {
+  const brillo = `brightness(${brightness.value}%) `;
+  const desenfoque = `blur(${blurFilter.value}px) `;
+  const contraste = `contrast(${contrast.value}%) `;
+  const grises = `grayscale(${grayscale.value}%) `;
+  const sepia2 = `sepia(${sepia.value}%) `;
+  const hue2 = `hue-rotate(${hue.value}deg) `;
+  const saturado = `saturate(${saturated.value}%) `;
+  const negativo = `invert(${negative.value}%) `;
 
-// Reset filters
-resetFilters.addEventListener('click', () => {
-  brightness.value = 100;
-  opacity.value = 100;
-  contrast.value = 100;
-  blurFilter.value = 0;
-  grayscale.value = 0;
-  sepia.value = 0;
-  hue.value = 0;
-  saturated.value = 100;
-  invert.value = 0;
-  negative.value = 0;
-  applyFilters();
+  const filtros = brillo + desenfoque + contraste + grises + sepia2 + hue2 + saturado + negativo;
+
+  if (imgMeme != null) {
+    imgMeme.style.filter = filtros;
+    imgMeme.style.opacity = opacity.value;
+
+  }
+}
+
+// Agregar event listeners a cada input
+brightness.addEventListener("input", addFilters);
+blurFilter.addEventListener("input", addFilters);
+contrast.addEventListener("input", addFilters);
+grayscale.addEventListener("input", addFilters);
+sepia.addEventListener("input", addFilters);
+hue.addEventListener("input", addFilters);
+saturated.addEventListener("input", addFilters);
+negative.addEventListener("input", addFilters);
+opacity.addEventListener("input", addFilters);
+
+//Evento resetear filtros
+resetFilters.addEventListener("click", function() {
+    imgMeme.style.filter = "none";
+
+    opacity.value = 1;
+    brightness.value = 1;
+    blurFilter.value = 0;
+    contrast.value = 0;
+    grayscale.value = 0;
+    sepia.value = 0;
+    hue.value = 0;
+    saturation.value = 100;
+    negative.value = 0;
+
+    resetFilters();
 });
+
 
 //Download Meme//
 const botonDescarga = document.getElementById('button-descarga');
