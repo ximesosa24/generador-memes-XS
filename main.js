f5c3cecf1d031e11d812c040506703f814eb18c1
ximesosa24 <ximenasosa999@gmail.com>
2024-07-19 06:45:01 -0300

    changes id checkbox, value to fonts and spacing add classes for js in css, change size icons, fixed js pickerColor, mixer, linespacing and added outline function

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //VARIABLES//
 const btn = document.querySelector('#btnDark'); //btn dark
-const body = document.querySelector('body'); //body 
+const body = document.querySelector('body'); //body
 const header = document.querySelector('#dark-mode'); //id mode dark
 const url = document.getElementById('url-link'); //Input de la URL
 const brightness = document.getElementById('brightness'); //Filter brightness
@@ -18,12 +18,12 @@ const contenedorMeme = document.getElementById('canvaMeme');// div download meme
 const imgMeme = document.getElementById('img-meme'); // div img
 const btnClose = document.getElementById ('btnClose'); //btn to close aside img
 const btnCloseTxt = document.getElementById('btnCloseTxt') ; //btn to close aside txt
-const selectFont = document.getElementById('select-font');//btn fonts 
+const selectFont = document.getElementById('select-font');//btn fonts
 const titleAside = document.getElementById('titleAside');//title aside
 const textArea = document.getElementById('text-area');//input textarea aside
+const textarea = document.getElementById('text-Area');// text area inferior
 const topText = document.getElementById('topText');// h5 top text
 const bottomText =  document.getElementById('bottomText'); // h5 bottom text
-const textarea = document.getElementById('text-Area');// text area inferior
 const pickerColor = document.getElementById ('selectColor')//color picker
 const selectMenu = document.getElementById('select-menu');//menu selector
 const checkbox = document.getElementById ('checkbox.txt');  // checkbox aside img
@@ -36,9 +36,15 @@ const bgColor = document.getElementById('input-bg-color'); //bg color
 const btnNinguno = document.getElementById('ninguno'); //btn ningún contorno
 const btnClaro = document.getElementById('claro');//btn claro contorn
 const btnOscuro = document.getElementById('oscuro'); //btn oscuro contorn
-const inputSpacing = document.getElementById('inputSpacing'); //input text spacing 
+const inputSpacing = document.getElementById('inputSpacing'); //input text spacing
 const containerTop = document.querySelector('.container-text'); //container top text
 const containerBottom = document.querySelector('.container-bottom'); //container bottom text
+const textAreaSup = document.getElementById('text-area-sup');//input textarea aside sup
+const textAreaInf = document.getElementById('text-area-inf');// text area inferior
+const checkBoxSup = document.getElementById('checkbox-sup')//checkbox sup
+const checkBoxInf = document.getElementById ('checkbox-inf')//checkbox inf
+const supTxt = document.getElementById('container-text-sup');//container top text
+const infTxt = document.getElementById('container-bottom-inf') ; // container bottom text
 
 //Event dark mode - it doesn't works correctly//
 btn.addEventListener('click', function(){
@@ -49,18 +55,20 @@ btn.addEventListener('click', function(){
   }
   });
 
-
-
 //ASIDES//
 
-document.getElementById('btnTxt').addEventListener('click', function() {
-  document.getElementById('asideTexto').classList.toggle('hidden');
+const btnTxt = document.getElementById('btnTxt');
+const asideTxt = document.getElementById('asideTexto');
+
+btnTxt.addEventListener('click', function() {
+  asideTxt.classList.toggle('hidden');
 });
 
-document.getElementById('btnImg').addEventListener('click', function() {
+const btnImg = document.getElementById('btnImg');
+const asideImg = document.getElementById('asideImg');
+btnImg.addEventListener('click', function() {
   document.getElementById('asideImg').classList.toggle('hidden');
 });
-
 
 //Function to close aside img and txt
 
@@ -89,17 +97,17 @@ url.addEventListener("change", function() {
 });
 
 
-// Picker color 
-pickerColor.addEventListener('change', function() {
-    const selectedColor = this.value;
-    divMeme.style.backgroundColor = selectedColor;
+// Picker color
+pickerColor.addEventListener('input', function() {
+  const selectedColor = this.value;
+  imgMeme.parentNode.style.backgroundColor = selectedColor;
 });
 
+
 //Event mixer img
-selectMenu.addEventListener('change', function() {
-  const selectedBlendMode = this.value;
-  imgMeme.style.mixBlendMode = selectedBlendMode;
-});
+const actualizarMixer = (evento) => {
+  imgMeme.style.backgroundBlendMode = evento.target.value
+};
 
 
 //Function add filters
@@ -155,28 +163,38 @@ resetFilters.addEventListener("click", function() {
 
 
 //Event textarea replace top text
-textArea.addEventListener('input', function() {
-  const text = textArea.value;
+textAreaSup.addEventListener('input', function() {
+  const text = textAreaSup.value;
   const title = titleAside.textContent.replace('texto', '');
-  topText.textContent = text; 
+  topText.textContent = text;
 });
-  
+
 //Event textArea replace bottom text
-textarea.addEventListener('input',  function(){
-  const text2 = textarea.value;
+textAreaInf.addEventListener('input',  function(){
+  const text2 = textAreaInf.value;
   const title2 = bottomText.textContent.replace('texto', '');
   bottomText.textContent = text2;
 });
 
-//Event change font family - it doesn't work correctly
+//Checkbox text superior and inferior
+checkBoxSup.addEventListener("click", function() {
+    supTxt.classList.toggle("hidden");
+});
+
+
+checkBoxInf.addEventListener("click", function() {
+    infTxt.classList.toggle("hidden");
+});
+
+//Event change font family //
   selectFont.addEventListener('change', function() {
     const selectedFont = selectFont.value;
-    topText.style.fontFamily = selectedFont.value;
-    bottomText.style.fontFamily = selectedFont.value;
+    topText.style.fontFamily = selectedFont;
+    bottomText.style.fontFamily = selectedFont;
   });
 
 
-//Event for change the font size
+//Event for change the font size //
 fontNumber.addEventListener('input', function() {
     const fontSize = this.value;
     topText.style.fontSize = fontSize + 'px';
@@ -199,15 +217,28 @@ alignRight.addEventListener('click', function(){
   bottomText.style.justifyContent = 'right';
 });
 
-// Change text color 
-textColor.addEventListener('change', function() {
+
+//Transparent - no anda//
+document.addEventListener("DOMContentLoaded", function() {
+  const checkTransparente = document.getElementById('checkboxTransp');
+
+  checkTransparente.addEventListener("click", function () {
+    supTxt.classList.toggle("transparent");
+    infTxt.classList.toggle("transparent");
+  });
+});
+
+
+// Change text color
+textColor.addEventListener('input', function() {
   const selectedColor = this.value;
   topText.style.color = selectedColor;
   bottomText.style.color = selectedColor;
 });
 
-//Change to background text color 
-bgColor.addEventListener('change', function(){
+
+//Change to background text color
+bgColor.addEventListener('input', function(){
   const selectedColor = this.value;
   containerTop.style.backgroundColor = selectedColor;
   containerBottom.style.backgroundColor = selectedColor;
@@ -223,52 +254,49 @@ const actualizarEspaciado = () => {
 inputSpacing.addEventListener('input', actualizarEspaciado);
 
 
-//CONTORN  //
-//   function eliminarClasesDeContorno() {
-//   topText.classList.remove('contorno-ninguno', 'contorno-claro', 'contorno-oscuro');
-//   bottomText.classList.remove('contorno-ninguno', 'contorno-claro', 'contorno-oscuro');
-// }
-
-// Agrega los event listeners a los botones REVISAR
-// btnNinguno.addEventListener('click', function (){
-//   eliminarClasesDeContorno();
-//   topText.classList.add('contorno-ninguno');
-//   bottomText.classList.add('contorno-ninguno');
-// });
-
-// btnClaro.addEventListener('click', function () {
-//   eliminarClasesDeContorno();
-//   topText.classList.add('contorno-claro');
-//   bottomText.classList.add('contorno-claro');
-// });
-
-// btnOscuro.addEventListener('click', function () {
-//   eliminarClasesDeContorno();
-//   topText.classList.add('contorno-oscuro');
-//   bottomText.classList.add('contorno-oscuro');
-// }); 
-//revisar//
-
-
 //LineSpacing //
+
 const lineSpacing = document.getElementById('lineSpacing');
-const actualizarInterlineado = () => {
-  const lineHeight = lineSpacing.value
-  topText.style.lineHeight = lineHeight + px;
-  bottomText.style.lineHeight = lineHeight + px;
+lineSpacing.addEventListener("change", (e) => {
+    topText.style.lineHeight = e.target.value;
+    bottomText.style.lineHeight = e.target.value;
+});
+
+//Contorn//
+const ningunButton = document.getElementById('ningun');
+const claroButton = document.getElementById('claro');
+const oscuroButton = document.getElementById('oscuro');
+
+  ningunButton.addEventListener('click', () => {
+    actualizarContorno('ningun');  });
+
+  claroButton.addEventListener('click', () => {
+    actualizarContorno('claro');
+  });
+
+  oscuroButton.addEventListener('click', () => {
+    actualizarContorno('oscuro');
+  });
+
+  function actualizarContorno(contorno) {
+  const grosor = '2px';
+  const shadowStyles = {
+    ninguno: 'none',
+    claro: `${grosor} ${grosor} #FFF, -${grosor} ${grosor} #FFF, ${grosor} -${grosor} #FFF, -${grosor} -${grosor} #FFF`,
+    oscuro: `${grosor} ${grosor} #000, -${grosor} ${grosor} #000, ${grosor} -${grosor} #000, -${grosor} -${grosor} #000`
+  };
+
+  topText.style.textShadow = shadowStyles[contorno];
+  bottomText.style.textShadow = shadowStyles[contorno];
 };
-lineSpacing.addEventListener('input', actualizarInterlineado);
-
-
-
 
 //Download Meme//
 
 function descargarMeme () {
     domtoimage.toBlob(contenedorMeme).then(function (blob) {
-        saveAs(blob, 'mi-meme.png')
-    }
-    )
-}
-
+        saveAs(blob, 'mi-meme.png');
+    });
+};
 botonDescarga.addEventListener('click', descargarMeme);
+
+
